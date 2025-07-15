2d0d460bb532b4b02ca253a17a213a3befffdef8
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-07-14 20:54:26 -0500

    endswith_python
    endswith_python

---
+++ end/ejemplo.py
@@ -0,0 +1,9 @@
+# Se crea una variable llamada 'parrafo' que contiene una cadena de texto.
+parrafo = 'hola julian velez G.'
+
+# Se utiliza el método 'endswith' para verificar si la cadena 'parrafo' termina con un punto (".")
+# Este método devuelve True si la cadena termina con ese carácter, de lo contrario devuelve False.
+final = parrafo.endswith(".")
+
+# Se imprime el resultado de la verificación anterior (True o False).
+print(final)
